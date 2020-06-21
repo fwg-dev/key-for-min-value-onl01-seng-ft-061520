f50fe75489788e0a9dae3908239a489165ccbc59
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-21 00:02:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-hash = {:blake => 500, :ashley => 2, :adam => 1}
-key_for_min_value(hash)
+# hash = {:blake => 500, :ashley => 2, :adam => 1}
+# key_for_min_value(hash)
 
+    lowest_key = nil
+    lowest_value = nil
+    hash.each do |key, value|
+      if lowest_value == nil || v < lowest_value
+        lowest_value = v
+        lowest_key = k
+      end
+    end
+    lowest_key
+  end
+4:50
+answe
 end
