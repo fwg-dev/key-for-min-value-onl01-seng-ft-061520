184279bdb6eb53e21bb4aa0488416094f4ef59ac
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-21 00:05:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 # key_for_min_value(hash)
 
-def completes(name_hash) # This also works for this lab.
+
 name_hash.sort do |x,y|
  x <=> y
 end.first[0]
