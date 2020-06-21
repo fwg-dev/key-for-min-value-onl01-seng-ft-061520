83a59bb09ddd6a676731109a87228cd30b42ff56
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-21 00:05:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,16 +5,8 @@ def key_for_min_value(name_hash)
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 # key_for_min_value(hash)
 
-    lowest_key = nil
-    lowest_value = nil
-    hash.each do |key, value|
-      if lowest_value == nil || v < lowest_value
-        lowest_value = v
-        lowest_key = k
-      end
-    end
-    lowest_key
-  end
-4:50
-answe
+def completes(name_hash) # This also works for this lab.
+name_hash.sort do |x,y|
+ x <=> y
+end.first[0]
 end
