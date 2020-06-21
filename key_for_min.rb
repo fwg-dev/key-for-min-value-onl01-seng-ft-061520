ad40ad9461bb41d9fbe658d960678c9efb0c9625
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-21 01:35:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,6 @@ def key_for_min_value(name_hash)
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 # key_for_min_value(hash)
 
-
-lowest_key = name_hash.reduce do |key, value|
-  key.last > value.last ? value : key
-end
-  if name_hash == {}
-    lowest_key
-  else
-    lowest_key.first
-  end
+  values = name_hash.values
+  hash.keys[values.index(values.min).to_i]
 end
